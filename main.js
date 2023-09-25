196e87b4b012cdbfe7d73e1fd6a71030f8c1f816
Laila <91194082+Lailaismyname@users.noreply.github.com>
2023-09-25 07:55:40 +0000

    Update main.js

--- main.js
+++ main.js
@@ -15,44 +15,32 @@ window.addEventListener('load', ()=>{
     const okBtn = document.querySelector('#okBtn');
     const colorBtn = document.querySelectorAll('.colorBtn');
     let color = '#ABF1C8';
-        //functie die grid maakt
         function makeGrid(size){
             size = size;
             for(let i = 0; i < (size*size); i++){
                 const box = document.createElement('div');
                 box.classList.add('box');
-                box.id = `box${i}`;         //heeft dit een id nodig? laten staan want is handig om te weten voor in de toekomst
+                box.id = `box${i}`;         
                 sketchBox.appendChild(box);
                 sketchBox.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
             }
         }
-        //functie die random nummer teruggeeft
         function randomNumber(){
             let ranNum = Math.random() * (255 - 0) + 1;
             return Math.trunc(ranNum);
         }
-
-        //bij start maak grid van 16*16
         makeGrid(16);       
-
-        // onhover moet de achtergrondkleur van het blokje veranderen 
         sketchBox.addEventListener(evenement, (event)=>{
                 if(event.target.classList.contains('box')){
                     event.target.style.background = color;
                 }
             });
-
-        // als er op gridsize knop word gedrukt, 
         okBtn.addEventListener('click', (event)=>{
             //pak waarde
             let gridSize = document.querySelector('#gridInput').value;
-            //empty de sketchbox
             sketchBox.innerHTML = '';
-            //maak nieuwe grid
             makeGrid(gridSize);
         })
-
-        //als er een kleur geselecteerd is, wijs nieuwe kleur aan color toe. 
         colorBtn.forEach(item =>{
             item.addEventListener('click', (event)=>{
                 console.log(navigator.platform);
@@ -71,5 +59,3 @@ window.addEventListener('load', ()=>{
             })
         })
 })
-
-//extra als het op mobiel is dan tekenen met onclick want hover bestaat niet volgens mij? eerst ff testen wat t doet?
